97b0c3e1da6e3415d389dad46d3e466a1bc64178
Василий <vasilygalaktionov@gmail.com>
2022-04-02 09:34:39 +0300

    Финал

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
